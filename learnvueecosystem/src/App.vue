73c6c3ba7a2c9ba143c8d26ff9d4e8d91498f70e
wuscier <1352288123@qq.com>
2019-01-07 23:24:02 +0800

    add progress bar

--- learnvueecosystem/src/App.vue
+++ learnvueecosystem/src/App.vue
@@ -1,22 +1,46 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
+  <div class="progress">
+    <div id='bar'></div>
   </div>
+
+<button @click="animateProgressBar">animate progress bar</button>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import $ from 'jquery'
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  methods:{
+    animateProgressBar(){
+      $('#bar').removeClass('transition');
+            $('#bar').css({
+        'width':'0%',
+        'background-color':'transparent'
+      });
+
+setTimeout(()=>{
+      $('#bar').addClass('transition');
+      $('#bar').css({
+        'width':'100%',
+        'background-color':'black'
+      });
+},100);
+
+
+    }
   }
 }
 </script>
 
-<style>
+<style  scoped>
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +49,20 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
+.progress {
+  height: 16px;
+  background-color: silver;
+}
+
+#bar{
+  height: 16px;
+}
+
+.transition{
+    transition: 0.4s linear;
+  transition-property: width, background-color;
+}
 </style>
+
